7dea1bfef5b42e8b486c98cd2466ee91e3f97875
Emily Klassen <emily@10kc.com>
2022-04-26 18:07:49 -0700

    feat: convert rgb to hex before printing

--- iterm2_colors2rgb/__main__.py
+++ iterm2_colors2rgb/__main__.py
@@ -3,11 +3,19 @@ import sys
 
 from iterm2_colors2rgb import iterm2_colors2rgb
 
+
+def toHex(c):
+    r, g, b = c
+    return hex((r << 16) | (g << 8) | b)[2:].zfill(6)
+
+
 if __name__ == "__main__":
     filepath = sys.argv[1] if len(sys.argv) > 1 else None
 
     try:
-        print(iterm2_colors2rgb(filepath))
+        bg, fg, ansi, bold = iterm2_colors2rgb(filepath)
+        print((toHex(bg), toHex(fg), [toHex(c)
+              for c in ansi], [toHex(c) for c in bold]))
 
     except Exception as exc:
         sys.exit(str(exc))
